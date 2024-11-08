06849006b3ec4bd10c2cd5e2f166b0ad73c674e6
KhacThien88 <KTeightop1512@gmail.com>
2024-11-08 18:46:56 +0700

    update env

--- src/utils/axiosUtils.ts
+++ src/utils/axiosUtils.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosUtils = axios.create({
-	baseURL: process.env.REACT_APP_API,
+	baseURL: "https://www.hcmuskhacthien.somee.com/api",
 	timeout: 10000,
 	headers: {
 		"Content-Type": "application/json",
