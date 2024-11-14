13596f1663be1de84845fcd029e99673b331aa1c
KhacThien88 <KTeightop1512@gmail.com>
2024-11-14 15:05:50 +0700

    update api link

--- src/utils/axiosUtils.ts
+++ src/utils/axiosUtils.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosUtils = axios.create({
-	baseURL: "https://www.devkhacthien.somee.com/api",
+	baseURL: `${process.env.REACT_APP_API}`,
 	timeout: 10000,
 	headers: {
 		"Content-Type": "application/json",
